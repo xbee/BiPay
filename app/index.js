ee698b89b9872086b3bbcc20ef30fffa5e3ae1cc
huanglifeng <hfeeki@qkldx.net>
2017-09-26 21:48:04 +0800

    change home

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import Navigator from './routes/stackNavigator';
+import StackNavigator from './routes/stackNavigator';
 
 export default class App extends Component {
     render() {
-        return <Navigator />;
+        return <StackNavigator />;
     }
 }
